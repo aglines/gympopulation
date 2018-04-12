565c70294e6cad3a0002a2cc62311b236b37bb46
Andrew Glines <aglines@gmail.com>
2018-04-12 13:19:16 +0100

    shifted chrome driver path to live in secrets file

--- dbtest.py
+++ dbtest.py
@@ -5,23 +5,15 @@ from secrets import *
 
 db = sqlite3.connect(dbLocation)
 cursor = db.cursor()
-# make sqlite3 return bytestrings not unicode
-# db.text_factory = bytes
-
 currPop = '142'
-
 currTime = time.time()
-# currTime = str(currTime)
-print "curr time", currTime
 
 cursor.execute('INSERT INTO times (timestamp, pop) VALUES (?,?)', (currTime, currPop))
 db.commit()
 
-
+# look at stored data once it's captured
 cursor.execute("SELECT timestamp FROM times WHERE id = 2")
 result = cursor.fetchone()[0]
-print result
 newresult = datetime.datetime.fromtimestamp(result)
 print "newresult = ", newresult
-
 db.close()
--- scrapenumber.py
+++ scrapenumber.py
@@ -7,8 +7,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import re
 import sqlite3
+import time
+import datetime
 
-driver = webdriver.Chrome("C:\chromedriver_win32\chromedriver.exe")
+driver = webdriver.Chrome(driverLocation)
 mainUrl = 'https://www.puregym.com/login/'
 driver.get(mainUrl)
 
@@ -20,15 +22,28 @@ passwordField.send_keys(userpassword)
 submitButton.click()
 
 # implicit wait
-driver.implicitly_wait(10)
+driver.implicitly_wait(5)
 currPop = driver.find_element_by_xpath('//*[@id="main-content"]/div[2]/div/div/div[1]/div/div/div/div[1]/div/p[1]/span')
 
 #regex to find the number portion of the captured text
 pattern = re.compile('\d+')
 matchedNumber = pattern.match(currPop.text)
-print matchedNumber.group()
+# print matchedNumber.group()
+currPop = matchedNumber.group()
 
 #sounds like db will be best route
+db = sqlite3.connect(dbLocation)
+cursor = db.cursor()
+currTime = time.time()
+cursor.execute('''INSERT INTO times (timestamp, pop)
+                VALUES (?,?)''', (currTime, currPop))
+db.commit()
 
+# look at stored data once it's captured
+# cursor.execute("SELECT timestamp FROM times WHERE id = 2")
+# result = cursor.fetchone()[0]
+# newresult = datetime.datetime.fromtimestamp(result)
+# print "newresult = ", newresult
 
-
+driver.quit()
+db.close()
