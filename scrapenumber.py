66af1cfe4eb2c56c09617922b321472361375c7a
Andrew Glines <aglines@gmail.com>
2018-04-16 11:42:47 +0100

    catch case where regex doesn't begin with digits

--- scrapenumber.py
+++ scrapenumber.py
@@ -29,29 +29,46 @@ except:
     logEvent('webdriver error')
 
 
-# get to the URL, log in, get data
+# get to the URL
 try:
     mainUrl = 'https://www.puregym.com/login/'
     driver.get(mainUrl)
+except:
+    errorLog = 'Error while getting mainURL'
+    logEvent(errorLog)
+
+# log in
+try:
     loginField = driver.find_element_by_xpath('//*[@id="email"]')
     passwordField = driver.find_element_by_xpath('//*[@id="pin"]')
     submitButton = driver.find_element_by_xpath('//*[@id="login-submit"]')
     loginField.send_keys(username)
     passwordField.send_keys(userpassword)
     submitButton.click()
-    # implicit wait
+except:
+    errorLog = 'Error during login'
+    logEvent(errorLog)
+
+# scrape data
+try:
     driver.implicitly_wait(5)
     currPop = driver.find_element_by_xpath(
         '//*[@id="main-content"]/div[2]/div/div/div[1]/div/div/div/div[1]/div/p[1]/span')
-    #regex to find the number portion of the captured text
-    pattern = re.compile('\d+')
-    matchedNumber = pattern.match(currPop.text)
-    # print matchedNumber.group()
-    currPop = matchedNumber.group()
-    logEvent('logged in, found currPop element: ' + currPop)
 except:
-    errorLog = 'Error during url get, login, or data scrape'
+    errorLog = 'Error during data scrape'
     logEvent(errorLog)
+
+pattern = re.compile('\d+')
+patternFewer = re.compile('Fewer than \d+')
+
+matchedNumber = pattern.match(currPop.text)
+matchedFewer = pattern.match(currPop.text)
+
+if (matchedNumber):
+    currPop = matchedNumber.group()
+
+elif (matchedFewer):
+    currPop = matchedFewer.group()
 
 try:
     currTime = time.time()
@@ -60,6 +77,7 @@ try:
     cursor.execute('''INSERT INTO times (timestamp, pop, readabletimestamp)
                     VALUES (?,?,?)''', (currTime, currPop, stringifiedCurrTime))
     db.commit()
+    logEvent('Driver/site/login/data/db all OK.  Current count: ' + currPop)
 except:
     errorLog = 'error during db insert statement'
     logEvent(errorLog)
