96eb05b36a46d142d0d67012feeb15ceedc2aa21
Andrew Glines <aglines@gmail.com>
2018-04-18 07:15:24 +0100

    error handling for gym survey page

--- scrapenumber.py
+++ scrapenumber.py
@@ -10,7 +10,6 @@ import sqlite3
 import time
 from datetime import datetime
 
-
 db = sqlite3.connect(dbLocation)
 cursor = db.cursor()
 
@@ -27,7 +26,6 @@ try:
     driver = webdriver.Chrome(driverLocation)
 except:
     logEvent('webdriver error')
-
 
 # get to the URL
 try:
@@ -49,9 +47,20 @@ except:
     errorLog = 'Error during login'
     logEvent(errorLog)
 
+driver.implicitly_wait(5)
+
+# get past survey if it exists
+try:
+    skipButton = driver.find_element_by_xpath('//*[@id="canSkip"]')
+    skipButton.click()
+    notActuallyAnError = 'Survey found. '
+    # logEvent(notActuallyAnError)
+except:
+    errorLog = 'survey not encountered, nothing to log'
+    # logEvent(errorLog)
+
 # scrape data
 try:
-    driver.implicitly_wait(5)
     currPop = driver.find_element_by_xpath(
         '//*[@id="main-content"]/div[2]/div/div/div[1]/div/div/div/div[1]/div/p[1]/span')
 except:
@@ -59,34 +68,31 @@ except:
     logEvent(errorLog)
 
 pattern = re.compile('\d+')
-patternFewer = re.compile('Fewer than \d+')
+patternFewer = re.compile('Fewer than 20 people')
+patternMore = re.compile('More than .')
 
 matchedNumber = pattern.match(currPop.text)
-matchedFewer = pattern.match(currPop.text)
+matchedFewer = patternFewer.match(currPop.text)
+matchedMore = patternMore.match(currPop.text)
 
 if (matchedNumber):
     currPop = matchedNumber.group()
-
 elif (matchedFewer):
     currPop = matchedFewer.group()
+elif (matchedMore):
+    currPop = matchedMore.group()
 
 try:
     currTime = time.time()
     stringifiedCurrTime = str(datetime.now())
-    #print "curr time = ", stringifiedCurrTime
     cursor.execute('''INSERT INTO times (timestamp, pop, readabletimestamp)
                     VALUES (?,?,?)''', (currTime, currPop, stringifiedCurrTime))
     db.commit()
-    logEvent('Driver/site/login/data/db all OK.  Current count: ' + currPop)
+    logEvent('All OK. Current pop: ' + currPop)
 except:
     errorLog = 'error during db insert statement'
     logEvent(errorLog)
 
-# look at stored data once it's captured
-# cursor.execute("SELECT timestamp FROM times WHERE id = 2")
-# result = cursor.fetchone()[0]
-# print "new result = ", datetime.datetime.fromtimestamp(result)
-
-#SHUT IT DOWN
+#shut it down
 db.close()
 driver.quit()
