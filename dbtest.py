b1cbc2932263d25537159901c943b4e0dcd63db7
Andrew Glines <aglines@gmail.com>
2018-06-17 11:53:28 +0100

    add dbtest.py

--- dbtest.py
+++ dbtest.py
@@ -1,19 +1,10 @@
-import sqlite3
-import time
-import datetime
-from secrets import *
+#dbtest.py
 
+
+import sqlite3
 db = sqlite3.connect(dbLocation)
 cursor = db.cursor()
-currPop = '142'
-currTime = time.time()
+# make sqlite3 return bytestrings not unicode
+db.text_factory = bytes
 
-cursor.execute('INSERT INTO times (timestamp, pop) VALUES (?,?)', (currTime, currPop))
-db.commit()
 
-# look at stored data once it's captured
-cursor.execute("SELECT timestamp FROM times WHERE id = 2")
-result = cursor.fetchone()[0]
-newresult = datetime.datetime.fromtimestamp(result)
-print "newresult = ", newresult
-db.close()
